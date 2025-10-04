9a9192d5e988a725425f1848e5de9a16abddfb2e
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-10-03 21:04:30 -0600

    Se le agrego funcionalidad a todas la rutas de usuarios y se redujo repeticion por medio de la variable tableName y el objeto userRoutes

--- src/backend/routes/usuarios.js
+++ src/backend/routes/usuarios.js
@@ -1,10 +1,13 @@
 import { app, connection } from "../server/server.js";
+import { userRoutes } from "./routes.js";
 
-export async function setUsersRoute() {
-    app.post("/postData", (request, response) => {
+const tableName = "usuarios";
+
+async function setUsersRoutes() {
+    app.post(userRoutes.postData, (request, response) => {
         const { nombre, email, password, telefono, direccion } = request.body;
 
-        const insertQuery = `INSERT INTO usuarios (nombre, email, password, telefono, direccion ) VALUES ($1, $2, $3, $4, $5)`;
+        const insertQuery = `INSERT INTO ${tableName} (nombre, email, password, telefono, direccion ) VALUES ($1, $2, $3, $4, $5)`;
         connection.query(
             insertQuery,
             [nombre, email, password, telefono, direccion],
@@ -18,8 +21,8 @@ export async function setUsersRoute() {
         );
     });
 
-    app.get("/fetchData", (request, response) => {
-        const query = "SELECT * FROM usuarios";
+    app.get(userRoutes.fetchData, (request, response) => {
+        const query = `SELECT * FROM ${tableName}`;
         connection.query(query, (error, result) => {
             if (error) {
                 response.send(error);
@@ -28,55 +31,53 @@ export async function setUsersRoute() {
             }
         });
     });
+
+    app.get(`${userRoutes.fetchById}/:id`, (request, response) => {
+        const id = request.params.id;
+        const fetchQuery = `SELECT * FROM ${tableName} WHERE id = $1`;
+
+        connection.query(fetchQuery, [id], (error, result) => {
+            if (error) {
+                response.send(error);
+            } else {
+                response.send(result.rows[0]);
+            }
+        });
+    });
+
+    app.put(`${userRoutes.update}/:id`, (request, response) => {
+        const id = request.params.id;
+
+        const { nombre, email, password, telefono, direccion } = request.body;
+
+        const updateQuery = `UPDATE ${tableName} SET nombre = $2, email = $3, password = $4, telefono = $5, direccion = $6 WHERE id = $1`;
+
+        connection.query(
+            updateQuery,
+            [id, nombre, email, password, telefono, direccion],
+            (error, result) => {
+                if (error) {
+                    response.send(error);
+                } else {
+                    response.send("UPDATED");
+                }
+            }
+        );
+    });
+
+    app.delete(`${userRoutes.delete}/:id`, (request, response) => {
+        const id = request.params.id;
+
+        const deleteQuery = `DELETE FROM ${tableName} WHERE id = $1 `;
+
+        connection.query(deleteQuery, [id], (error, result) => {
+            if (error) {
+                response.send(error);
+            } else {
+                response.send("DELETED ROW");
+            }
+        });
+    });
 }
 
-// app.get("/fetchById/:id", (request, response) => {
-//     const id = request.params.id;
-//     const fetchQuery = "SELECT * FROM orders WHERE id = $1";
-
-//     connection.query(fetchQuery, [id], (error, result) => {
-//         if (error) {
-//             response.send(error);
-//         } else {
-//             console.log(response);
-//             response.send(result.rows[0]);
-//         }
-//     });
-// });
-
-// app.put("/update/:id", (request, response) => {
-//     const id = request.params.id;
-
-//     const { name, quantity, price, address } = request.body;
-
-//     const updateQuery =
-//         "UPDATE orders SET name = $2, quantity = $3, price = $4, address = $5 WHERE id = $1";
-
-//     connection.query(
-//         updateQuery,
-//         [id, name, quantity, price, address],
-//         (error, result) => {
-//             if (error) {
-//                 response.send(error);
-//             } else {
-//                 console.log(result);
-//                 response.send("UPDATED");
-//             }
-//         }
-//     );
-// });
-
-// app.delete("/delete/:id", (request, response) => {
-//     const id = request.params.id;
-
-//     const deleteQuery = "DELETE FROM orders WHERE id = $1 ";
-
-//     connection.query(deleteQuery, [id], (error, result) => {
-//         if (error) {
-//             response.send(error);
-//         } else {
-//             console.log(result);
-//             response.send("DELETED ROW");
-//         }
-//     });
-// });
+export { setUsersRoutes };
