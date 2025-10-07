eed1badcb2042c2f75f64ab6eb87629d35d80264
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-10-06 21:49:06 -0600

    Moved location of .env file and made it work

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -1,4 +1,6 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import { Client } from "pg";
 import express from "express";
 import { setUsersRoutes } from "../routes/usuarios.js";
@@ -7,6 +9,11 @@ import { setCarritoItemsRoutes } from "../routes/carrito_items.js";
 import { setPedidoItemsRoutes } from "../routes/pedido_items.js";
 import { setPedidosRoutes } from "../routes/pedidos.js";
 import { setProductosRoutes } from "../routes/productos.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 const app = express();
 app.use(express.json());
