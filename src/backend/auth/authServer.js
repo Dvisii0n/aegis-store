99b2f3dc67708bae99e4524dd2489666fd56400f
jorge <jorge@thinkpad.com>
2025-10-07 18:53:15 -0700

    Implemented simple login

--- src/backend/auth/authServer.js
+++ src/backend/auth/authServer.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import { setAuthRoutes } from "./authRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,5 +11,11 @@ dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 const authApp = express();
 authApp.use(express.json());
+
+setAuthRoutes();
+
+authApp.listen(4000, () => {
+    console.log("auth server running...");
+});
 
 export { authApp };
