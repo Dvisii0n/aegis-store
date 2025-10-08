c1fb6324d8b3b3c56ce0f1dca376980b21a98922
jorge <jorge@thinkpad.com>
2025-10-07 18:53:27 -0700

    Tested more

--- src/backend/index.js
+++ src/backend/index.js
@@ -15,7 +15,7 @@ const ejemploRequestBody = {
 // await users.createRow(ejemploRequestBody);
 
 // await users.updateRow(13, ejemploRequestBody);
-// console.log(await users.fetchByName("mehashearon"));
+console.log(await users.fetchByName("te"));
 
 // console.log(await users.fetchRows());
 // console.log(await productos.fetchRows());
