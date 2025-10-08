1f5dabbcdc7f93bc0d16ee43275c15bf8b477e4c
jorge <jorge@thinkpad.com>
2025-10-08 09:59:45 -0700

    Tested login

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,13 +1,15 @@
 import ProductosService from "./services/productosService.js";
 import UsuariosService from "./services/usuariosService.js";
+import AuthService from "./auth/authService.js";
 
 const users = new UsuariosService();
 const productos = new ProductosService();
+const auth = new AuthService();
 
 const ejemploRequestBody = {
-    // nombre: "mehashearon",
+    nombre: "tet",
     // email: "test2@gmail.com",
-    password: "hashthis",
+    // password: "passwrd",
     // telefono: "",
     // direccion: "",
 };
@@ -15,9 +17,11 @@ const ejemploRequestBody = {
 // await users.createRow(ejemploRequestBody);
 
 // await users.updateRow(13, ejemploRequestBody);
-console.log(await users.fetchByName("te"));
+console.log(await users.fetchByName("test"));
 
 // console.log(await users.fetchRows());
 // console.log(await productos.fetchRows());
 
 // console.log(await users.fetchByID(3));
+
+// console.log(await auth.userLogin(ejemploRequestBody));
