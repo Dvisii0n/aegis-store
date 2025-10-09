eba2ea528b7c8a44e43190ac6cb1ab78226da4d7
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-10-08 22:13:27 -0600

    Se testearon funciones de authService

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,22 +6,27 @@ const users = new UsuariosService();
 const productos = new ProductosService();
 const auth = new AuthService();
 
-const ejemploRequestBody = {
-    nombre: "tet",
-    // email: "test2@gmail.com",
-    // password: "passwrd",
-    // telefono: "",
-    // direccion: "",
+const ejemploUsuario = {
+    nombre: "registro4",
+    // email: "test@gmail.com",
+    password: "password",
 };
 
-// await users.createRow(ejemploRequestBody);
+const ejemploRegistro = {
+    nombre: "registro4",
+    email: "registro4@gmail.com",
+    password: "password",
+    telefono: "",
+    direccion: "",
+};
 
-// await users.updateRow(13, ejemploRequestBody);
-console.log(await users.fetchByName("test"));
+const ejemploRefreshToken = {
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTksIm5vbWJyZSI6InJlZ2lzdHJvNCIsImVtYWlsIjoicmVnaXN0cm80QGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiJDJiJDEwJC5DTTkzVzVUNE5tR2JZQXJqVzltSU9hODZ2MG52bnpTQlRUZlh4eW1vS21MUVNqUEMydjQyIiwidGVsZWZvbm8iOiIiLCJkaXJlY2Npb24iOiIiLCJyb2wiOiJjbGllbnRlIiwiZmVjaGFfY3JlYWNpb24iOiIyMDI1LTEwLTA5VDA0OjA1OjQ5LjgwMloiLCJpYXQiOjE3NTk5ODI3Nzl9.Ujdy-T1634R38vUU-fBAPyYauYNTv0owm50xzN0wmC8",
+};
+// console.log(await auth.userLogin(ejemploUsuario));
 
-// console.log(await users.fetchRows());
-// console.log(await productos.fetchRows());
+// console.log(await auth.getNewAccessToken(ejemploRefreshToken));
 
-// console.log(await users.fetchByID(3));
+console.log(await auth.logout(ejemploRefreshToken));
 
-// console.log(await auth.userLogin(ejemploRequestBody));
+// console.log(await auth.registerUser(ejemploRegistro));
