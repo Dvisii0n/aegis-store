f49baba6bfff4c765b327644f8014e6c48a138c3
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-10-08 22:16:12 -0600

    Se implementaron todos los endpoints de autenticacion, junto con una funcion de middleware hecha para autenticar tokens, lista para agregarse a todos los servicios

--- src/backend/auth/auth.js
+++ src/backend/auth/auth.js
@@ -1,33 +1,154 @@
 import { authApp } from "./authServer.js";
-import jsonwebtoken from "jsonwebtoken";
+import pkg from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import { authRoutes } from "./authRoutes.js";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { connection } from "../server/server.js";
 
-import UsuariosService from "../services/usuariosService.js";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-async function setAuthRoutes() {
-    const usersService = new UsuariosService();
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
+const { sign, verify } = pkg;
+
+//Tokens perro tokens sin tokens no se hace NADA
+async function setAuthRoutes(authRoutes) {
     authApp.post(authRoutes.login, async (req, res) => {
-        const user = await usersService.fetchByName(req.body.nombre);
+        const fetchQuery = `SELECT * FROM usuarios WHERE nombre = $1`;
 
-        if (user === null) {
-            res.statusMessage = "User does not exist";
-            return res.status(400).send();
-        }
-
-        try {
-            if (await bcrypt.compare(req.body.password, user.password)) {
-                res.statusMessage = "Login Succesful";
-                res.send();
-            } else {
-                res.statusMessage = "Password does not match user";
-                res.send();
+        connection.query(fetchQuery, [req.body.nombre], async (err, result) => {
+            if (err) {
+                return res.send(err);
             }
-        } catch (error) {
-            res.status(500).send();
-        }
+
+            const user = result.rows[0];
+
+            if (user === undefined) {
+                res.statusMessage = "User does not exist";
+                return res.status(400).send();
+            }
+
+            try {
+                if (await bcrypt.compare(req.body.password, user.password)) {
+                    const accessToken = generateAccessToken(user);
+                    const refreshToken = sign(
+                        user,
+                        process.env.REFRESH_TOKEN_SECRET
+                    );
+
+                    res.send({
+                        msg: "Login Succesful",
+                        accessToken: accessToken,
+                        refreshToken: refreshToken,
+                    });
+                    //guarda el refresh token en la bd
+                    connection.query(
+                        "INSERT INTO refreshtokens (token) VALUES ($1)",
+                        [refreshToken],
+                        (err, result) => {
+                            if (err) {
+                                return res.send(err);
+                            }
+                        }
+                    );
+                } else {
+                    res.statusMessage = "Password does not match user";
+                    res.status(400).send();
+                }
+            } catch (error) {
+                res.status(500).send;
+            }
+        });
+    });
+
+    //Reminder: esta funcion nomas se puede ejecutar al llenar el formulario de registro
+    authApp.post(authRoutes.signup, async (req, res) => {
+        const { nombre, email, password, telefono, direccion } = req.body;
+
+        const hashedPassword = await getPasswordHash(password);
+
+        const insertQuery = `INSERT INTO usuarios (nombre, email, password, telefono, direccion) VALUES ($1, $2, $3, $4, $5)`;
+        connection.query(
+            insertQuery,
+            [nombre, email, hashedPassword, telefono, direccion],
+            (error, result) => {
+                if (error) {
+                    res.send(error);
+                } else {
+                    res.send(result);
+                }
+            }
+        );
+    });
+
+    //mandar peticion aqui para refrescar token de acceso
+    authApp.post(authRoutes.token, async (req, res) => {
+        const refreshToken = req.body.token;
+        if (refreshToken === null) return res.sendStatus(401);
+
+        connection.query(
+            `SELECT * FROM refreshtokens WHERE token = $1`,
+            [refreshToken],
+            (err, result) => {
+                if (err) {
+                    return res.sendStatus(403);
+                }
+
+                verify(
+                    refreshToken,
+                    process.env.REFRESH_TOKEN_SECRET,
+                    (err, user) => {
+                        if (err) {
+                            return res.sendStatus(403);
+                        }
+
+                        const accessToken = generateAccessToken({
+                            name: user.nombre,
+                        });
+                        res.send({ accessToken: accessToken });
+                    }
+                );
+            }
+        );
+    });
+
+    authApp.delete("/logout", async (req, res) => {
+        connection.query(
+            "DELETE FROM refreshtokens WHERE token = $1",
+            [req.body.token],
+            (err, result) => {
+                if (err) {
+                    return res.send(error);
+                }
+
+                res.send(result);
+            }
+        );
     });
 }
 
-export { setAuthRoutes };
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (token === null) return res.sendStatus(401);
+
+    verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+function generateAccessToken(user) {
+    return sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10m" });
+}
+
+async function getPasswordHash(password) {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+}
+
+export { setAuthRoutes, authenticateToken };
