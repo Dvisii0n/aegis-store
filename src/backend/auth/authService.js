409305b33f7980c1e3cfaab249afafc0b03408ec
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-10-08 22:12:48 -0600

    Se implemeantaron las siguientes funciones: userLogin() para iniciar sesion, registerUser para registrar n nuevo usuario, getNewAccessToken para refrescar un token de acceso expirado y logout para eliminar el refresh token especificado

--- src/backend/auth/authService.js
+++ src/backend/auth/authService.js
@@ -12,7 +12,59 @@ export default class AuthService {
                 },
                 body: JSON.stringify(requestBody),
             });
-            return response.statusText;
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async registerUser(requestBody) {
+        const url = `${authBaseUrl}${authRoutes.signup}`;
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(requestBody),
+            });
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getNewAccessToken(refreshTokenObj) {
+        const url = `${authBaseUrl}${authRoutes.token}`;
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(refreshTokenObj),
+            });
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logout(refreshTokenObj) {
+        const url = `${authBaseUrl}${authRoutes.logout}`;
+        try {
+            const response = await fetch(url, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(refreshTokenObj),
+            });
+            const data = await response.json();
+            return data;
         } catch (error) {
             throw error;
         }
