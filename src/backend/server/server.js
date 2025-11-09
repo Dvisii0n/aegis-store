b3d0eb4c7e99e1138002fdaee31ba9bf5e753a57
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-11-09 15:46:47 -0600

    Se agrego permiso de CORS para testing local

--- src/backend/server/server.js
+++ src/backend/server/server.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { Client } from "pg";
@@ -17,6 +18,8 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 const app = express();
+
+app.use(cors({ origin: true, credentials: true }));
 app.use("/auth", authApp);
 app.use(express.json());
 
