40a8d9f097f0533cf84449c8f5582783936a8238
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-11-14 14:45:40 -0600

    Se agrego el archivo para los eventes del formualario de informacion de pago - El usuario puede realizar un pedido - Al realizar el pedido, se guardan los items correspondientes - Se actualiza la direccion y el telefono del usuario cuando ingresa informacion diferente

---
+++ src/frontend/events/paymentEvents.js
@@ -0,0 +1,70 @@
+import PedidosService from "../../backend/services/pedidosService.js";
+import PedidoItemsService from "../../backend/services/pedidoItemsService.js";
+import ProductosService from "../../backend/services/productosService.js";
+import UsuariosService from "../../backend/services/usuariosService.js";
+
+(async function setPaymentSubmitEvent() {
+    const order = new PedidosService();
+    const orderItem = new PedidoItemsService();
+    const prod = new ProductosService();
+    const user = new UsuariosService();
+    const form = document.querySelector(".payment-form");
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const data = new FormData(form);
+
+        const info = {
+            name: data.get("name"),
+            lastName: data.get("last-name"),
+            countryCode: data.get("country-code"),
+            phoneNum: data.get("phone-num"),
+            address: data.get("address"),
+            city: data.get("city"),
+            state: data.get("state"),
+            postalCode: data.get("postal-code"),
+            country: data.get("country"),
+        };
+
+        const accessToken = localStorage.getItem("accessToken");
+        const userID = localStorage.getItem("userID");
+        const prodIDs = JSON.parse(localStorage.getItem("orderItems"));
+        const total = localStorage.getItem("total");
+
+        try {
+            await user.updateRow(
+                userID,
+                {
+                    telefono: `${info.countryCode} ${info.phoneNum}`,
+                    direccion: info.address,
+                },
+                accessToken
+            );
+
+            const orderID = await order.createRow(
+                { usuario_id: userID, total: total },
+                accessToken
+            );
+
+            for (let product of prodIDs) {
+                const prodInfo = await prod.fetchByID(product.id, accessToken);
+                console.log(prodInfo);
+                await orderItem.createRow(
+                    {
+                        pedido_id: orderID,
+                        producto_id: product.id,
+                        cantidad: product.quantity,
+                        precio_unitario: prodInfo.precio,
+                    },
+                    accessToken
+                );
+            }
+
+            window.location.href = "../pages/order.html";
+        } catch (error) {
+            console.log(error);
+            alert(error);
+        }
+    });
+})();
