23c92deefd254fc7ed96e1015e5c7c69094a0a73
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-11-13 21:41:00 -0600

    Se agrego evento de eliminar del carrito

---
+++ src/frontend/events/cartEvents.js
@@ -0,0 +1,17 @@
+import CarritoItemsService from "../../backend/services/carritoItemsService.js";
+import { renderCartItems } from "../scripts/cart.js";
+
+export function setDeleteCartItemEvents() {
+    const cartItems = new CarritoItemsService();
+    const removeBtns = document.querySelectorAll(".far.fa-times-circle");
+
+    removeBtns.forEach((btn) => {
+        btn.addEventListener("click", async (e) => {
+            const dataID =
+                btn.parentNode.parentNode.getAttribute("data-cart-item-id");
+            const accessToken = localStorage.getItem("accessToken");
+            await cartItems.deleteRow(dataID, accessToken);
+            await renderCartItems();
+        });
+    });
+}
