2a2d64cf563c8878e5a089746d78e7198108f3d0
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-11-13 21:39:02 -0600

    Se agrego evento de agregar a carrito

--- src/frontend/events/shopEvents.js
+++ src/frontend/events/shopEvents.js
@@ -1,3 +1,6 @@
+import CarritosService from "../../backend/services/carritosService.js";
+import CarritoItemsService from "../../backend/services/carritoItemsService.js";
+
 export function setShowModalEvent() {
     const prodCards = Array.from(document.querySelectorAll(".pro"));
 
@@ -29,7 +32,44 @@ export function setCloseModalEvent() {
                 `.P${modals.indexOf(modal)}`
             );
 
-            console.log(currentModal);
+            currentModal.close();
+        });
+    });
+}
+
+export function setAddToCartEvent() {
+    const cart = new CarritosService();
+    const cartItems = new CarritoItemsService();
+    const modals = Array.from(document.querySelectorAll("dialog"));
+    modals.forEach((modal) => {
+        const addToCartBtn = document.querySelector(
+            `.P${modals.indexOf(modal)}>#prodetails>.single-pro-details>.normal`
+        );
+
+        const userID = localStorage.getItem("userID");
+        const accessToken = localStorage.getItem("accessToken");
+        addToCartBtn.addEventListener("click", async (e) => {
+            const userCart = await cart.fetchByUserID(userID, accessToken);
+
+            const productID = modal.parentNode.getAttribute("data-id");
+            const currentModal = document.querySelector(
+                `.P${modals.indexOf(modal)}`
+            );
+            const inputVal = document.querySelector(
+                `.P${modals.indexOf(
+                    modal
+                )}>#prodetails>.single-pro-details>input`
+            ).value;
+
+            await cartItems.createRow(
+                {
+                    carrito_id: userCart.id,
+                    producto_id: productID,
+                    cantidad: inputVal,
+                },
+                accessToken
+            );
+
             currentModal.close();
         });
     });
