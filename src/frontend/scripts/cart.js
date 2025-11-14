29cabbd13dbc84327415026ccae5a2d39d7f9c40
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2025-11-13 21:41:33 -0600

    Se agrego funcion de agregar productos al carrito y de renderizar los productos agregados

---
+++ src/frontend/scripts/cart.js
@@ -0,0 +1,105 @@
+import CarritosService from "../../backend/services/carritosService.js";
+import CarritoItemsService from "../../backend/services/carritoItemsService.js";
+import ProductosService from "../../backend/services/productosService.js";
+import { setDeleteCartItemEvents } from "../events/cartEvents.js";
+
+async function getCartItemsInfo() {
+    const cart = new CarritosService();
+    const cartItems = new CarritoItemsService();
+    const prods = new ProductosService();
+    const userID = localStorage.getItem("userID");
+    const accessToken = localStorage.getItem("accessToken");
+    const userCart = await cart.fetchByUserID(userID, accessToken);
+
+    const cartItemsInfo = await cartItems.fetchByCartID(
+        userCart.id,
+        accessToken
+    );
+
+    const itemsInfo = [];
+
+    for (let item of cartItemsInfo) {
+        const prodInfo = await prods.fetchByID(item.producto_id, accessToken);
+        prodInfo["quantity"] = item.cantidad;
+        prodInfo["itemCartID"] = item.id;
+        itemsInfo.push(prodInfo);
+    }
+
+    return itemsInfo;
+}
+
+function createCartItemDisplay(prodInfo) {
+    const tr = document.createElement("tr");
+    tr.setAttribute("data-cart-item-id", prodInfo.itemCartID);
+
+    const tdRemove = document.createElement("td");
+
+    const icon = document.createElement("i");
+    icon.className = "far fa-times-circle";
+    tdRemove.appendChild(icon);
+
+    const tdImg = document.createElement("td");
+    const img = document.createElement("img");
+    img.src = prodInfo.imagen_url;
+    img.alt = "";
+    tdImg.appendChild(img);
+
+    const tdTitle = document.createElement("td");
+    tdTitle.textContent = prodInfo.nombre;
+
+    const tdPrice = document.createElement("td");
+    tdPrice.textContent = `MXN$${prodInfo.precio}`;
+
+    const tdQty = document.createElement("td");
+    const quantityDiv = document.createElement("div");
+    quantityDiv.textContent = prodInfo.quantity;
+    tdQty.appendChild(quantityDiv);
+
+    const tdTotal = document.createElement("td");
+    const sub = prodInfo.precio * prodInfo.quantity;
+    tdTotal.textContent = `MXN$${sub}`;
+    tdTotal.className = "subtotal-prod";
+    tdTotal.setAttribute("subtotal-amount", sub);
+
+    tr.appendChild(tdRemove);
+    tr.appendChild(tdImg);
+    tr.appendChild(tdTitle);
+    tr.appendChild(tdPrice);
+    tr.appendChild(tdQty);
+    tr.appendChild(tdTotal);
+
+    return tr;
+}
+
+export async function renderCartItems() {
+    const itemsInfo = await getCartItemsInfo();
+    const container = document.querySelector("tbody");
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for (let item of itemsInfo) {
+        const prodRow = createCartItemDisplay(item);
+        container.appendChild(prodRow);
+    }
+
+    const subtotals = document.querySelectorAll(".subtotal-prod");
+
+    let cartSubtotal = 0;
+    subtotals.forEach((subtotal) => {
+        cartSubtotal += Number.parseFloat(
+            subtotal.getAttribute("subtotal-amount")
+        );
+    });
+
+    const cartSub = document.querySelector(".cart-subtotal");
+    const cartTotal = document.querySelector(".cart-total");
+
+    cartSub.textContent = `MXN$${cartSubtotal}`;
+
+    cartTotal.textContent = `MXN$${cartSubtotal}`;
+
+    setDeleteCartItemEvents();
+}
+
+renderCartItems();
